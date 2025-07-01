c7787e77300a8235f703c7849799a0ed2b80bf48
gigun-dev <117321963+gigun-dev@users.noreply.github.com>
2025-07-01 15:50:43 +0900

    update for led

--- experimentFFT/experimentFFT.ino
+++ experimentFFT/experimentFFT.ino
@@ -4,9 +4,21 @@
 #define F_S 9600
 ArduinoFFT<float> FFT; // double → float へ
 float vReal[N], vImag[N];
+const int ledPin =13;
+const uint8_t TARGET_BIN = 40;  // 3kHzに相当するピン番号
+#define THRESHOLD 23 // 振幅しきい値
 
 void setup() {
   Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW); // 初期状態で消灯
+  // Wiring check: blink LED twice at startup
+  for (int i = 0; i < 2; i++) {
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    delay(500);
+  }
   ADCSRA = (ADCSRA & 0b11111000) | 0x04; // prescaler=16
 }
 
@@ -21,6 +33,13 @@ void loop() {
   FFT.windowing(vReal, N, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
   FFT.compute(vReal, vImag, N, FFT_FORWARD);
   FFT.complexToMagnitude(vReal, vImag, N);
+
+
+  if(vReal[TARGET_BIN] > THRESHOLD){
+    digitalWrite(ledPin, HIGH); //しきい値を超えたら点灯
+  } else{
+    digitalWrite(ledPin,LOW); //それ以外は消灯
+  }
 
   /* 3. 出力 */
   for (uint16_t i = 1; i < N / 2; i++) {
